301bb290f562551bcf21b10c83aa7caf11f76f0e
LutfulloAsqarov <lutfulloasqarov9@gmail.com>
2024-12-16 16:11:43 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import { Fragment, useEffect, useState } from "react";
-import Home from "./pages/home";
+import { Fragment, lazy, Suspense } from "react";
 import Layout from "./layout";
 import { Route, Router, Routes } from "react-router-dom";
 import { IoMdArrowRoundUp } from "react-icons/io";
+import Loader from "./components/Loader";
+
+//lazy loading
+
+const Home = lazy(() => import("./pages/home/index"));
 
 function App() {
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
-            behavior: "smooth", // Animatsiya bilan scroll qiladi
+            behavior: "smooth",
         });
     };
     return (
         <Fragment>
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </Layout>
+            <Suspense fallback={<Loader />}>
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </Layout>
+            </Suspense>
             <div className="back-top" onClick={scrollToTop}>
                 <IoMdArrowRoundUp />
             </div>
---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss"
+
+const Loader = () => {
+    return <div className="loader">Landing</div>;
+};
+
+export default Loader;
